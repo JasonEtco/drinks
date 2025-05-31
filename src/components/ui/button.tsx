c85481426a3439b56a076a3dc7566c4aab1f1252
Jason Etcovitch <jasonetco@gmail.com>
2025-05-31 14:11:42 -0400

    Tweak ghost button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "border bg-background shadow-xs hover:bg-accent hover:text-secondary-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/40",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost: "hover:bg-accent/30 hover:text-accent-foreground",
+        ghost: "hover:bg-primary/30",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
@@ -31,7 +31,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  },
+  }
 );
 
 function Button({
